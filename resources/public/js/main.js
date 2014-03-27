26aeb5f26b71f569627f2269ce03ff210f094f98
pld <peter@helioid.com>
2014-03-27 13:04:55 -0400

    PLD: remove generated js

--- resources/public/js/main.js
+++
@@ -1,8 +0,0 @@
-goog.addDependency("base.js", ['goog'], []);
-goog.addDependency("../cljs/core.js", ['cljs.core'], ['goog.string', 'goog.array', 'goog.object', 'goog.string.StringBuffer']);
-goog.addDependency("../om/dom.js", ['om.dom'], ['cljs.core']);
-goog.addDependency("../om/core.js", ['om.core'], ['cljs.core', 'om.dom']);
-goog.addDependency("../clojure/walk.js", ['clojure.walk'], ['cljs.core']);
-goog.addDependency("../clojure/string.js", ['clojure.string'], ['cljs.core', 'goog.string', 'goog.string.StringBuffer']);
-goog.addDependency("../cljs/reader.js", ['cljs.reader'], ['cljs.core', 'goog.string']);
-goog.addDependency("../om_viewer/core.js", ['om_viewer.core'], ['cljs.core', 'goog.events.EventType', 'goog.dom', 'om.core', 'clojure.walk', 'clojure.string', 'om.dom', 'cljs.reader', 'goog.net.XhrIo', 'goog.events']);
